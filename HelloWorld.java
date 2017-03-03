a38ca23706ed015ee1d964ef9d5910e56ef7fa8d
Zhouweihua <nk_zhouwh@163.com>
2017-03-03 16:07:18 +0800

    my commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+
+public class HelloWorld{
+	 public static void main(String[] args){
+	 	  final String a$="Hello ";
+	 	  int m=011;
+	 	  float m1=1.23e1f;
+	 	  float m2=12.3f;
+	 	  char a='b';
+	 	  byte b=(byte)a;
+	 	  System.out.println(a$);
+	 	  System.out.print(b);
+	}
+}
