6ab18e749043ca15ce1b7047b3655f19d7f96380
4079481 <4079481@qq.com>
2017-03-05 22:10:04 +0800

    helloworld and sort

---
+++ sort.java
@@ -0,0 +1,15 @@
+/*
+用Java编写一个能对数组比较的程序，需要输出按行升序排序的结果 如输入
+1 2 5 4 8
+输出：
+1 2 4 5 8
+要求：数组是通过sort程序的参数输入。
+*/
+public class sort{
+	 public static void main(String[] args){
+         //将args转换为int数组
+         
+         //对int数组排序后输出结果
+	 	 
+	}
+}
